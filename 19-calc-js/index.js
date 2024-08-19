d258b8805f6cafc53d38f2333e76f0786da98b32
Aluízio Oliveira Gonçalves Filho <80135392+aluizi0@users.noreply.github.com>
2024-08-19 20:19:25 -0300

    Dígito númerico e exclusão de numero(Calculadora)

---
+++ 19-calc-js/index.js
@@ -0,0 +1,30 @@
+const main = document.querySelector('main');
+const root = document.querySelector(':root');
+const input = document.getElementById('input');
+const resultInput = document.getElementById('result');
+
+// impedir que o usuario digite letras
+
+const allowedKeys = ['0','1','2','3','4','5','6','7','8','9','+','-','*','/','(',')','.'];
+
+/**
+ * Event listener for the input element to prevent the user from typing letters.
+ * 
+ * Ouvinte de evento para o elemento input para evitar que o usuário digite letras.
+ * 
+ * @param {Event} ev - O objeto de evento keydown.
+ * @returns {void}
+ */
+
+input.addEventListener('keydown', (ev) => {
+    ev.preventDefault();
+    if (allowedKeys.includes(ev.key)){
+        input.value += ev.key;
+        return;
+    }
+    if (ev.key === 'Backspace'){
+        input.value = input.value.slice(0, -1);
+    }
+});
+
+
