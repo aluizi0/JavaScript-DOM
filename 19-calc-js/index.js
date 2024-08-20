489c2d03562c69bc38b58bd0546f5d611a953ed7
Aluízio Oliveira Gonçalves Filho <80135392+aluizi0@users.noreply.github.com>
2024-08-20 10:22:48 -0300

    Implementando ERRO

--- 19-calc-js/index.js
+++ 19-calc-js/index.js
@@ -36,8 +36,13 @@ input.addEventListener('keydown', (ev) => {
 document.getElementById('equal').addEventListener('click', calculate);
 
 function calculate(){
+    resultInput.value = 'ERROR';
+    resultInput.classList.add('error');
+
     const result = eval(input.value);
+    
     resultInput.value = result;
+    resultInput.classList.remove('error');
 }
 
 document.getElementById('copyToClipboard').addEventListener('click', function(ev){
